c09dd8503ca7f917f59e3667142d971b5cc3432e
Aviral Goel <goel.aviral@gmail.com>
2023-09-08 18:47:36 -0700

    Add Window Configuration

--- init.el
+++ init.el
@@ -1,14 +1,15 @@
 (org-babel-load-file
- (expand-file-name "config.org" user-emacs-directory))
+    (expand-file-name "config.org" user-emacs-directory))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("3e2039156049bd0661317137a3761d4c2ff43e8a2aa423f6db0c0e8df0197492" default))
+      '("3e2039156049bd0661317137a3761d4c2ff43e8a2aa423f6db0c0e8df0197492" default))
  '(package-selected-packages
-   '(evil-surround evil-collection kind-icon dirvish orderless vertico marginalia dashboard)))
+      '(ace-window winum pulsar moody pacmacs evil-surround evil-collection kind-icon dirvish orderless vertico marginalia dashboard)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
