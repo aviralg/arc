0f807c4e127550576abaac7ed9b2d2aa480425cf
Aviral Goel <goel.aviral@gmail.com>
2023-12-27 14:15:39 -0800

    Numerous UI changes and configuration for new packages

--- init.el
+++ init.el
@@ -1,2 +1,16 @@
 (org-babel-load-file
     (expand-file-name "config.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+      '(writeroom-mode mood-line ws-butler which-key vundo volatile-highlights visual-regexp vertico undo-fu ts toc-org tiny tempel-collection symbol-overlay string-inflection speed-type shell-pop rg rainbow-mode racket-mode pulsar pdf-view-restore pcmpl-args page-break-lines osx-trash org-modern orderless multiple-cursors modus-themes minions marginalia magit-todos magit-lfs lispy kind-icon keycast ialign htmlize ht highlight-numbers goto-line-preview git-timemachine git-modes git-messenger git-link gif-screencast forge flycheck-pos-tip flycheck-color-mode-line fix-word expand-region exec-path-from-shell esup eshell-z eshell-syntax-highlighting eshell-info-banner eshell-fringe-status eshell-did-you-mean eshell-bookmark esh-help esh-buf-stack esh-autosuggest emms embark-consult elfeed-tube-mpv elfeed-org dumb-jump drag-stuff doom-modeline docker dirvish diff-hl devdocs define-word deadgrep dashboard dash-at-point corfu copy-as-format consult-project-extra consult-flycheck consult-eglot consult-dir consult-compile-multi compile-multi-embark cheatsheet cape avy-zap anzu)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t :box (:style released-button)))))
+(put 'upcase-region 'disabled nil)
