5c86feaabb896ff5f41b5b71c5c2fbe57b727dee
Aviral Goel <goel.aviral@gmail.com>
2024-09-05 08:06:05 -0700

    Add org-modern and pdf-tools. Disable desktop-save-mode.

--- init.el
+++ init.el
@@ -256,7 +256,8 @@
 
 
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Saving-Emacs-Sessions.html
-(desktop-save-mode 1)
+;; TODO - this causes problems with writeroom-mode
+;; (desktop-save-mode 1)
 
 
 ;; SAVE HISTORY
@@ -1900,6 +1901,9 @@
 ;; TODO pdf-tools
 ;; TODO pdf-view-restore
 
+(use-package pdf-tools
+  :ensure t)
+
 
 ;; EPUB
 
@@ -1950,6 +1954,38 @@
 ;; https://github.com/rlister/org-present
 (use-package org-present
   :ensure t)
+
+;; https://github.com/minad/org-modern
+(use-package org-modern
+  :ensure t
+  :config
+  (setq
+   ;; Edit settings
+   org-auto-align-tags nil
+   org-tags-column 0
+   org-catch-invisible-edits 'show-and-error
+   org-special-ctrl-a/e t
+   org-insert-heading-respect-content t
+
+   ;; Org styling, hide markup etc.
+   org-hide-emphasis-markers t
+   org-pretty-entities t
+
+   ;; Agenda styling
+   org-agenda-tags-column 0
+   org-agenda-block-separator ?─
+   org-agenda-time-grid
+   '((daily today require-timed)
+     (800 1000 1200 1400 1600 1800 2000)
+     " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+   org-agenda-current-time-string
+   "◀── now ─────────────────────────────────────────────────")
+
+  ;; Ellipsis styling
+  (setq org-ellipsis "…")
+  (set-face-attribute 'org-ellipsis nil :inherit 'default :box nil)
+
+  (global-org-modern-mode))
 
 
 ;; SHELL
